d44fe137761a198947ca9352db799491a47d7994
Lukas <buddilp90@gmail.com>
2020-07-31 22:46:06 +0200

    first commit

---
+++ src/main/java/de/hdskins/labymod/HdSkinsAddon.java
@@ -0,0 +1,25 @@
+package de.hdskins.labymod;
+
+import net.labymod.api.LabyModAddon;
+import net.labymod.settings.elements.SettingsElement;
+
+import java.util.List;
+
+public class HdSkinsAddon extends LabyModAddon {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void loadConfig() {
+
+    }
+
+    @Override
+    protected void fillSettings(List<SettingsElement> list) {
+
+    }
+
+}
