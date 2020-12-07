f7cbe0476b3b16eab7f727889c8d93c19ac2caf4
derklaro <pasqual.koschmieder@gmail.com>
2020-12-07 21:39:17 +0100

    Fix incorrect loading of non-hd skins

--- skin-manager-shared/src/main/java/de/hdskins/labymod/shared/asm/draw/SkullRenderer.java
+++ skin-manager-shared/src/main/java/de/hdskins/labymod/shared/asm/draw/SkullRenderer.java
@@ -24,6 +24,7 @@ import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import com.mojang.authlib.properties.Property;
 import de.hdskins.labymod.shared.MCUtil;
+import de.hdskins.labymod.shared.texture.HDSkinManager;
 import net.labymod.main.LabyMod;
 import net.labymod.utils.UUIDFetcher;
 import net.minecraft.client.Minecraft;
@@ -35,6 +36,8 @@ import net.minecraft.client.resources.SkinManager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
@@ -50,6 +53,11 @@ public final class SkullRenderer {
     private static final ExecutorService EXECUTOR_SERVICE = Executors.newCachedThreadPool();
     private static final Property PLACEHOLDER_PROPERTY = new Property("", EMPTY_PLACEHOLDER_PROPERTY_NAME);
     private static final Cache<MinecraftProfileTexture, ResourceLocation> CACHE = CacheBuilder.newBuilder()
+        .expireAfterAccess(30, TimeUnit.SECONDS)
+        .ticker(Ticker.systemTicker())
+        .concurrencyLevel(4)
+        .build();
+    private static final Cache<GameProfile, GameProfile> GAME_PROFILE_CACHE = CacheBuilder.newBuilder()
         .expireAfterAccess(30, TimeUnit.SECONDS)
         .ticker(Ticker.systemTicker())
         .concurrencyLevel(4)
@@ -104,23 +112,45 @@ public final class SkullRenderer {
     }
 
     private static ResourceLocation loadTextureResourceLocation(GameProfile profile) {
-        ResourceLocation resourceLocation = DefaultPlayerSkin.getDefaultSkinLegacy();
+        // hack:
+        // LabyMod calls this method for rendering the skulls in the main menu as well as in the chat using
+        // session.getProfile(). This method always generates a new GameProfile so we have to cache and
+        // re-use the old one, as this profile gets filled with the profile textures of the user
+        GameProfile gameProfile = GAME_PROFILE_CACHE.getIfPresent(profile);
+        if (gameProfile == null) {
+            GAME_PROFILE_CACHE.put(profile, gameProfile = profile);
+        }
+        return loadTextureResourceLocation0(gameProfile);
+    }
+
+    private static ResourceLocation loadTextureResourceLocation0(GameProfile profile) {
+        ResourceLocation resourceLocation = getSkin(profile);
         if (profile != null) {
             final SkinManager skinManager = Minecraft.getMinecraft().getSkinManager();
             Map<MinecraftProfileTexture.Type, MinecraftProfileTexture> map = skinManager.loadSkinFromCache(profile);
+            if (!(skinManager instanceof HDSkinManager) && (map.isEmpty() || !map.containsKey(MinecraftProfileTexture.Type.SKIN))) {
+                // hack:
+                // the default skin manager of minecraft caches the skins (profile - profile) but
+                // if the textures were set lazily the textures in the map are still empty so
+                // we have to get them now if they are set to not use the stupid cache from
+                // minecraft.
+                // also this check if not needed if we use our internal skin manager as we keep
+                // track of these changes and maintain them in our backing cache
+                map = Minecraft.getMinecraft().getSessionService().getTextures(profile, false);
+            }
+
             if (map.containsKey(MinecraftProfileTexture.Type.SKIN)) {
-                ResourceLocation cachedLocation = CACHE.getIfPresent(map.get(MinecraftProfileTexture.Type.SKIN));
+                final ResourceLocation cachedLocation = CACHE.getIfPresent(map.get(MinecraftProfileTexture.Type.SKIN));
                 if (cachedLocation != null) {
                     return cachedLocation;
                 } else {
-                    skinManager.loadSkin(map.get(MinecraftProfileTexture.Type.SKIN), MinecraftProfileTexture.Type.SKIN, (type, location, texture) -> {
+                    resourceLocation = skinManager.loadSkin(map.get(MinecraftProfileTexture.Type.SKIN), MinecraftProfileTexture.Type.SKIN, (type, location, texture) -> {
                         if (type == MinecraftProfileTexture.Type.SKIN) {
                             CACHE.put(texture, location);
                         }
                     });
                 }
-            } else if (profile.getProperties().isEmpty()
-                || (!profile.getProperties().containsKey("textures") && !profile.getProperties().containsKey(EMPTY_PLACEHOLDER_PROPERTY_NAME))) {
+            } else if (profile.getProperties().isEmpty() || (!profile.getProperties().containsKey("textures") && !profile.getProperties().containsKey(EMPTY_PLACEHOLDER_PROPERTY_NAME))) {
                 profile.getProperties().put(EMPTY_PLACEHOLDER_PROPERTY_NAME, PLACEHOLDER_PROPERTY); // Set to prevent duplicate lookups
                 EXECUTOR_SERVICE.execute(() -> Minecraft.getMinecraft().getSessionService().fillProfileProperties(profile, false));
             } else {
@@ -130,5 +160,12 @@ public final class SkullRenderer {
         }
 
         return resourceLocation;
+    }
+
+    @Nonnull
+    private static ResourceLocation getSkin(@Nullable GameProfile profile) {
+        return profile == null || profile.getId() == null
+            ? DefaultPlayerSkin.getDefaultSkinLegacy()
+            : DefaultPlayerSkin.getDefaultSkin(profile.getId());
     }
 }
